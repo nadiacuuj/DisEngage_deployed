50b05dcb1435a2dda61b2689476776e5f5733010
givechoco <80017561+GiveChoco@users.noreply.github.com>
2024-11-13 10:50:38 -0500

    fixed eventcard padding

--- frontend/src/component/EventCard.js
+++ frontend/src/component/EventCard.js
@@ -2,11 +2,11 @@ function EventCard({event}){
     console.log(event);
     return(
     <div class="w-fit h-fit shadow-md hover:shadow-2xl border overflow-hidden bg-slate-100 rounded-lg">
-        <img class= "object-cover h-48 w-full" src = {event.ImageSrc} alt = "Event image"></img>
-        <div class='w-full px-2'>
-            <h4 class="text-lg">{event.EventTitle}</h4>
-            <p class="text-sm">{event.EventLocation}</p>
-            <p class="text-sm">{event.EventTime}</p>
+        <img class= "object-cover h-40 w-full" src = {event.ImageSrc} alt = "Event image"></img>
+        <div class='w-full'>
+            <p class="text-lg leading-tight p-2 m-2 max-w-[331px] break-words">{event.EventTitle}</p>
+            <p class="text-sm px-2 mx-2">{event.EventLocation}</p>
+            <p class="text-sm px-2 mx-2 pb-2 mb-2">{event.EventTime}</p>
         </div>
     </div>
     )
