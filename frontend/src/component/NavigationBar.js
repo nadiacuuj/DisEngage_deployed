6eba65aec21deab26f21fdc35ca523fcc3b0d564
givechoco <80017561+GiveChoco@users.noreply.github.com>
2024-11-21 16:13:16 -0500

    added logo

--- frontend/src/component/NavigationBar.js
+++ frontend/src/component/NavigationBar.js
@@ -5,7 +5,7 @@ function Navigationbar(){
     return(
         <nav class="flex items-center justify-between flex-wrap bg-violet-800 p-6">
     <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <img class="fill-current h-12 w-fill mr-2 bg-white"  src="https://logos-world.net/wp-content/uploads/2021/09/NYU-Logo.png"></img>
+        <img class="fill-current h-12 w-fill mr-2 bg-white"  src="/logo.png"></img>
         <span class="font-semibold text-xl tracking-tight">DisEngage</span>
     </div>
     <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
